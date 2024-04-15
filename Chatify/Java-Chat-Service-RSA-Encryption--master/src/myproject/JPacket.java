e536a8e6ad180c7cfa3720ddcbbb6efae3b66388
nadiaitgithub <143493278+nadiaitgithub@users.noreply.github.com>
2024-04-15 21:35:47 +0600

    Add files via upload

---
+++ Chatify/Java-Chat-Service-RSA-Encryption--master/src/myproject/JPacket.java
@@ -0,0 +1,13 @@
+package myproject;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class JPacket implements Serializable {
+    JMessage core;
+    Timestamp timestamp;
+    public JPacket(JMessage input) {
+        timestamp = new Timestamp((new Date()).getTime());
+        core = input;
+    }
+}
